97da7366379fc57cccbf261262325dd15378200b
Shreekirtana <130212298+Shreekirtana@users.noreply.github.com>
2023-06-08 14:49:13 +0530

    case.sh

---
+++ case.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+case $1 in
+1) echo "This is one"
+;;
+2) echo "This is two"
+;;
+3) echo "This is three"
+;;
+*) echo "This is invalid"
+;;
+esac
--- fact.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-fact=1
-while [ $1 -gt 0 ]
-do
-fact=`expr $fact \* $1`
-1=`expr $1 - 1`
-done
-echo " the factorial value is $fact"
